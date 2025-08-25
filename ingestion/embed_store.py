94884ad32d8453131a212623b2eecef48c6116f2
segni_abera <segniab69@gmail.com>
2025-08-25 14:03:28 +0300

    updated some erros

--- ingestion/embed_store.py
+++ ingestion/embed_store.py
@@ -1,4 +1,8 @@
 # ingestion/embed_store.py
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 
 from load_and_clean import load_all_pdfs, clean_text
 from chunk_text import chunk_text
